950de60a26e2d0cbccbc3175fd2881bbdf1dd482
山口弦 <yamaguchi.gen@bpsinc.jp>
2020-04-23 13:21:31 +0900

    test

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="/css/index.css" media="screen" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="/js/index.js"></script>
+    <title>テスト</title>
+</head>
+
+<body>
+    油圧
+</body>
+
+</html>
---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    const insert = document.createElement("div");
+    insert.innerHTML = "jsで追加ですが";
+    document.body.appendChild(insert);
+}
